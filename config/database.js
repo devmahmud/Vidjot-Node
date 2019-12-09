f830db54fad416f36191662ddef9d4bc59823466
devmahmud <expelmahmud@gmail.com>
2019-12-09 14:21:48 +0600

    mongo uri fixed

--- config/database.js
+++ config/database.js
@@ -1,7 +1,6 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
-    mongoURI:
-      "mongodb+srv://mahmud:mahmud12345@cluster0-9zx2s.mongodb.net/test?retryWrites=true&w=majority"
+    mongoURI: "your connection string"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/vidjot-dev" };
